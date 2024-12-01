cd0ae037f6240d959bc5c819e36e6ca89ed84e39
abbymarks <abbymarks6@gmail.com>
2024-12-01 15:12:07 -0500

    8

--- Spotify-Wrapped-CS2340/spotify_wrapped/spotify_wrapped/settings.py
+++ Spotify-Wrapped-CS2340/spotify_wrapped/spotify_wrapped/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,10 +53,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = "spotify_wrapped.urls"
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
