b4326600d0f1198955b0393f9381c50f2d944a39
abbymarks <abbymarks6@gmail.com>
2024-12-01 17:14:39 -0500

    8

--- spotify_wrapped/spotify_wrapped/settings.py
+++ spotify_wrapped/spotify_wrapped/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +80,8 @@ WSGI_APPLICATION = "spotify_wrapped.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 }
 
 
@@ -127,6 +126,7 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
