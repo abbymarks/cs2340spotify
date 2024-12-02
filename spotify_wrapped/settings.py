6a22a473fc999d9c42d7a15c577d188af47ad558
abbymarks <abbymarks6@gmail.com>
2024-12-02 17:10:29 -0500

    grs

--- spotify_wrapped/settings.py
+++ spotify_wrapped/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
